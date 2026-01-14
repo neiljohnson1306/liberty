debe1b7bf5fb35f5f73d7a832814f838615da5f8
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-01-14 18:42:05 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
 import Contact from './pages/Contact'
+import CaseStudies from "./pages/CaseStudies"
+
 
 export default function App() {
   return (
@@ -15,6 +17,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/case-studies" element={<CaseStudies />} />
       </Routes>
       <Footer />
     </>

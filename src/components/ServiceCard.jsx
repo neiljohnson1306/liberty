d90ec06a7b24bd560bef670c83748b32cabd40cb
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-01-14 18:56:40 +0000

    Update ServiceCard.jsx

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,6 +1,6 @@
 export default function ServiceCard({ title, desc }) {
   return (
-    <div className="border bg-white dark:bg-darkCard p-6 rounded-xl hover:shadow-lg transition">
+   <div className="border bg-white dark:bg-darkCard p-6 md:p-8 rounded-xl hover:shadow-lg transition">
       <h3 className="text-xl font-semibold mb-2">{title}</h3>
       <p className="text-secondary dark:text-gray-300">{desc}</p>
     </div>
